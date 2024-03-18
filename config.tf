3e1943abe91f5d40e3959505b774177a97b56a27
Cloud User <ec2-user@ip-172-31-31-248.ec2.internal>
2024-03-18 23:01:56 +0000

    Updated terraform files

---
+++ config.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+	region = "us-east-1"
+        shared_credentials_files = ["/home/ec2-user/.aws/creds"]
+
+}
+
+#EC2 instance details
+
+resource "aws_instance" "Jenkins-Test" {
+        count = 1
+	ami = "ami-0fe630eb857a6ec83"
+	instance_type = "t2.micro"
+        key_name = "mybasicserver"
+	security_groups = ["launch-wizard-3"]
+	tags = {
+		Name = "Jenkins-Test1"
+	}
+
+}
